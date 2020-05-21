f93df73011898346662b232c593e38efdf324d21
wkwkgg <49357140+wkwkgg@users.noreply.github.com>
2020-05-21 12:52:05 +0900

    Update update_progress.sh

--- update_progress.sh
+++ update_progress.sh
@@ -9,7 +9,7 @@ git config --global user.name $name
 git config --global user.email $email
 
 git remote set-url origin https://${name}:${GITHUB_TOKEN}@github.com/tmu-nlp/100knock2020.git
-git checkout -b master
+git checkout master
 
 last_commit_msg="$(git log -1 | tail -1)"
 
@@ -17,5 +17,5 @@ if [ -z "$(echo $last_commit_msg | grep Bot)"]; then
     python3 make_progress.py
     git add progress.png
     git commit -m "[Bot] update progress bar"
-    git push origin HEAD
+    git push origin master
 fi
