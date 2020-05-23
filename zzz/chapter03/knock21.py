151305eafdbf22d61465aeb11b9bfc600f4091aa
zz_zhang <glgjs417yhfbqz>
2020-05-23 17:50:06 +0900

    modify knock21

--- zzz/chapter03/knock21.py
+++ zzz/chapter03/knock21.py
@@ -1,6 +1,10 @@
+import re
 from zzz.chapter03.knock20 import extract_text
 
 if __name__ == '__main__':
     filename = 'jawiki-country.json'
     text = extract_text(filename, 'イギリス')
-    print('\n'.join([line for line in text if 'Category' in line]))
+
+    pattern = r'\[\[Category:.*?\]\]'
+    categories = re.findall(pattern, '\n'.join(text))
+    print('\n'.join(categories))
