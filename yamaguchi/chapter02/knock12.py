866ac9ffe60c92bf8ee9feaf18caa33e361d35bc
GUCCHI-e <65330043+GUCCHI-e@users.noreply.github.com>
2020-05-28 14:38:28 +0900

    Add files via upload

--- yamaguchi/chapter02/knock12.py
+++ yamaguchi/chapter02/knock12.py
@@ -5,8 +5,9 @@ import pandas as pd
 # headerがないので「None」を指定
 data = pd.read_table('popular-names.txt', sep='\t', header=None)
 
-# 結果
-print("行数は，" + str(len(data)) + "行です．")
+# 1列目を「output_konck12_col1.txt」に，2列目を「output_konck12_col2.txt」に保存
+data[0].to_csv('output_knock12_col1.txt', index=False, header=None)
+data[1].to_csv('output_knock12_col2.txt', index=False, header=None)
 
 # 確認
-# wcコマンドの代わりとして「find /c /v "" popular-names.txt」を入力．
+# cutコマンドで文字列を切り出す
