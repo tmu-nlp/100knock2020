e7a6891926c6a251186c610b3cb2a38c84d32200
kyunashige <kyuna.b23@gmail.com>
2020-06-08 17:28:19 +0900

    Fix knock30, 37

--- kiyuna/chapter04/knock37.py
+++ kiyuna/chapter04/knock37.py
@@ -15,7 +15,6 @@ from typing import Dict, List
 import matplotlib.pyplot as plt
 
 from knock30 import mecab_into_sentences
-from knock35 import build_cnter
 
 Morpheme = Dict[str, str]
 Sentence = List[Morpheme]
@@ -24,19 +23,14 @@ Sentence = List[Morpheme]
 if __name__ == "__main__":
     tgt_key, tgt_val = "surface", "猫"
     num = 10
-    exclude_pos = {"助詞", "助動詞", "記号"}
 
     cnter = Counter()
     for sentence in mecab_into_sentences():
         if any(d[tgt_key] == tgt_val for d in sentence):
-            cnter += Counter(
-                d[tgt_key] for d in sentence  # if d["pos"] not in exclude_pos
-            )
+            cnter += Counter(d[tgt_key] for d in sentence)
     del cnter[tgt_val]
-    for k, v in build_cnter({"surface": "表層形"}).most_common(50):
-        del cnter[k]
-    labels, data = zip(*cnter.most_common(num))
 
+    labels, data = zip(*cnter.most_common(num))
     plt.bar(range(num), data)
     plt.title(f"「{tgt_val}」と共起頻度の高い上位 {num} 語")
     plt.xticks(range(num), map(lambda x: f'"{x}"', labels))
