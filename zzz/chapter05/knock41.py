37277092986b034d7aa3f19b8c3fe8bbf8bf1504
zz_zhang <glgjs417yhfbqz>
2020-06-18 11:54:50 +0900

    add comments in knock41, 49

--- zzz/chapter05/knock41.py
+++ zzz/chapter05/knock41.py
@@ -27,28 +27,33 @@ def load_chunk_from_file(file):
         temp_morphs = []
         current_index = 0
         for item in list(filter(lambda x: x != '', sentence.split('\n'))):
+            # * 0 -1D 1/1 0.000000
+            # * index dstD _ _
             if item[0] == '*':
+                # save morphs to current chunk
                 if len(temp_chunks) > 0:
                     temp_chunks[current_index].morphs += temp_morphs
                     temp_morphs = []
-
+                # save dst and current index
                 chunk = item.split(' ')
                 current_index = int(chunk[1])
                 dst = int(chunk[2][:-1])
-                # srcs = []
-
+                # new a chunk in chunk list for current chunk
                 if len(temp_chunks) <= current_index:
                     temp_chunks.append(Chunk([], 0, []))
                 temp_chunks[current_index].dst = dst
-
+                # new some chunks in chunk list for dst of current chunk
+                # add current chunk as src for dst chunk
                 if dst != -1:
                     while len(temp_chunks) <= dst:
                         temp_chunks.append(Chunk([], 0, []))
                     temp_chunks[dst].append_src(current_index)
 
+            # 人工	名詞,一般,*,*,*,*,人工,ジンコウ,ジンコー
             else:
                 morph = re.split(r'\t|,|\n', item)
                 temp_morphs.append(Morph(morph[0], morph[7], morph[1], morph[2]))
+        # add morphs for last chunk
         if len(temp_chunks) > 0:
             temp_chunks[-1].morphs += temp_morphs
 
--- zzz/chapter05/knock49.py
+++ zzz/chapter05/knock49.py
@@ -1,5 +1,5 @@
 from zzz.chapter05.knock41 import Chunk, load_chunk_from_file
-
+from zzz.chapter05.knock43 import find_pos
 
 def find_common_parent(chunk_list, index1, index2):
     visited = [index1, index2]
@@ -31,42 +31,49 @@ if __name__ == '__main__':
         chunks = load_chunk_from_file(file)
 
         for sentence in chunks:
+            # traversal all chunk pairs (chunk_i, chunk_j)
             for i in range(len(sentence)):
                 for j in range(i + 1, len(sentence)):
                     chunk_i = sentence[i]
                     chunk_j = sentence[j]
+                    if find_pos(chunk_i, '名詞') and find_pos(chunk_j, '名詞'):
+                        # replace noun in chunk_i and chunk_j with X and Y
+                        phrase_i = ''.join(['X' if m.pos == '名詞' else m.surface for m in chunk_i.morphs if m.pos != '記号'])
+                        # phrase_i = ''.join([m.surface for m in chunk_i.morphs if m.pos != '記号'])
+                        while 'XX' in phrase_i:
+                            phrase_i = phrase_i.replace('XX', 'X')
+                        phrase_j = ''.join(['Y' if m.pos == '名詞' else m.surface for m in chunk_j.morphs if m.pos != '記号'])
+                        # phrase_j = ''.join([m.surface for m in chunk_j.morphs if m.pos != '記号'])
+                        while 'YY' in phrase_j:
+                            phrase_j = phrase_j.replace('YY', 'Y')
+                        # find common parent node
+                        parent_index = find_common_parent(sentence, i, j)
+                        # chunk_i -> chunk_j
+                        if parent_index == j:
+                            print(phrase_i, end=' ')
+                            # node = chunk_i+1, ..., chunk_j-1
+                            node = sentence[chunk_i.dst]
+                            while node != chunk_j:
+                                print('->', ''.join([m.surface for m in node.morphs if m.pos != '記号']), end=' ')
+                                node = sentence[node.dst]
+                            print('->', phrase_j)
+                        # chunk_i -> chunk_k, chunk_j -> chunk_k
+                        else:
+                            print(phrase_i, end=' ')
+                            # node = chunk_i+1, ..., chunk_k-1
+                            node = sentence[chunk_i.dst]
+                            while node != sentence[parent_index]:
+                                print('->', ''.join([m.surface for m in node.morphs if m.pos != '記号']), end=' ')
+                                node = sentence[node.dst]
+                            print('|', end=' ')
 
-                    phrase_i = ''.join(['X' if m.pos == '名詞' else m.surface for m in chunk_i.morphs if m.pos != '記号'])
-                    # phrase_i = ''.join([m.surface for m in chunk_i.morphs if m.pos != '記号'])
-                    while 'XX' in phrase_i:
-                        phrase_i = phrase_i.replace('XX', 'X')
-                    phrase_j = ''.join(['Y' if m.pos == '名詞' else m.surface for m in chunk_j.morphs if m.pos != '記号'])
-                    # phrase_j = ''.join([m.surface for m in chunk_j.morphs if m.pos != '記号'])
-                    while 'YY' in phrase_j:
-                        phrase_j = phrase_j.replace('YY', 'Y')
+                            print(phrase_j, end=' ')
+                            # node = chunk_j+1, ..., chunk_k-1
+                            node = sentence[chunk_j.dst]
+                            while node != sentence[parent_index]:
+                                print('->', ''.join([m.surface for m in node.morphs if m.pos != '記号']), end=' ')
+                                node = sentence[node.dst]
+                            print('|', end=' ')
 
-                    parent_index = find_common_parent(sentence, i, j)
-                    if parent_index == j:
-                        print(phrase_i, '->', end=' ')
-                        node = sentence[chunk_i.dst]
-                        while node != chunk_j:
-                            print(''.join([m.surface for m in node.morphs if m.pos != '記号']), '->', end=' ')
-                            node = sentence[node.dst]
-                        print(phrase_j)
-                    else:
-                        print(phrase_i, end=' ')
-                        node = sentence[chunk_i.dst]
-                        while node != sentence[parent_index]:
-                            print('->', ''.join([m.surface for m in node.morphs if m.pos != '記号']), '->', end=' ')
-                            node = sentence[node.dst]
-                        print('|', end=' ')
-
-                        print(phrase_j, end=' ')
-                        node = sentence[chunk_j.dst]
-                        while node != sentence[parent_index]:
-                            print('->', ''.join([m.surface for m in node.morphs if m.pos != '記号']), end=' ')
-                            node = sentence[node.dst]
-                        print('|', end=' ')
-
-                        print(''.join([m.surface for m in sentence[parent_index].morphs if m.pos != '記号']))
+                            print(''.join([m.surface for m in sentence[parent_index].morphs if m.pos != '記号']))
             print('EOS')
