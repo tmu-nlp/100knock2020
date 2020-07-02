99fe4a5fe820e2f4797b806250c0c5ec079d755d
zz_zhang <glgjs417yhfbqz>
2020-07-02 14:50:10 +0900

    modify comments in knock62

--- zzz/chapter07/knock62.py
+++ zzz/chapter07/knock62.py
@@ -2,5 +2,5 @@ from gensim import models
 
 w = models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
-# Compute cosine similarity between two words.
+# Find the top-N most similar words.
 print(w.most_similar('United_States', topn=10))
