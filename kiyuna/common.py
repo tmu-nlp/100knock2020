e0c8e01f751ec97b5646b2319eda966e5e3f7e7d
kyunashige <kyuna.b23@gmail.com>
2020-08-13 01:26:44 +0900

    Add wrapper sample

--- kiyuna/common.py
+++ kiyuna/common.py
@@ -1,5 +1,29 @@
+from time import gmtime, sleep, strftime
 from typing import Any, List, Sequence, Tuple
 
 
 def n_gram(seq: Sequence[Any], n: int) -> List[Tuple[Any]]:
     return list(zip(*[seq[i:] for i in range(n)]))
+
+
+def print_time(func):
+    def wrapper(*args, **kwargs):
+        print("in  |", strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+        res = func(*args, **kwargs)
+        print("res |", res)
+        res[1]["return"] = 0
+        print("out |", strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+        return res
+
+    return wrapper
+
+
+@print_time
+def my_func(*args, **kwargs):
+    sleep(1)
+    return args, kwargs
+
+
+if __name__ == "__main__":
+    res = my_func("NLP", name="kyuna", ans=42)
+    print("res |", res)
